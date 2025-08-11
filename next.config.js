d6c7325585867af45fc225c4b45dfe1863cd3590
dot_gh <turchindimaua@gmail.com>
2025-08-12 01:21:15 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true
+  // Для пользовательского сайта (custom domain dotkit.me) basePath/assetPrefix НЕ нужны.
+  // Если бы это был project pages (user.github.io/repo), тогда:
+  // basePath: "/<repo-name>",
+  // assetPrefix: "/<repo-name>/"
 };
 module.exports = nextConfig;
