17317f368f34e22a773a924bd0b87f9cd17e9dc5
dot_gh <turchindimaua@gmail.com>
2025-08-12 12:54:12 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import type { AppProps } from "next/app";
 import "../styles/tailwind.css";
+import dynamic from "next/dynamic";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
