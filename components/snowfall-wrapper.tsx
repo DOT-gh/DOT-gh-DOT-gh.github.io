3d2eaa2081cd0cff6c805745a815014d8869ab2a
dot_gh <turchindimaua@gmail.com>
2025-12-14 01:35:15 +0200

    Delete components directory

--- components/snowfall-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { useAppState } from "@/lib/store"
-import { Snowfall } from "./snowfall"
-
-export function SnowfallWrapper() {
-  const { snowAmount } = useAppState()
-  return <Snowfall snowAmount={snowAmount} />
-}
